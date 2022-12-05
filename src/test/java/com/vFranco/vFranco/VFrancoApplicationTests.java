d5e79114cd1800efda17a2563384528d7429a082
josedavid-celsor <josedavidrosalesrios0310@gmail.com>
2022-12-05 19:03:55 +0100

    first commit

---
+++ src/test/java/com/vFranco/vFranco/VFrancoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vFranco.vFranco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VFrancoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
