b0375e0262838621b8a733d3e782ba15cc5e2c51
josedavid-celsor <josedavidrosalesrios0310@gmail.com>
2022-12-17 16:56:21 +0100

    Aiuda Policia

--- src/main/java/com/vFranco/vFranco/service/AuthService.java
+++ src/main/java/com/vFranco/vFranco/service/AuthService.java
@@ -81,8 +81,8 @@ public class AuthService implements UserDetailsService {
 
     // Para encriptar el password
     String encryptedPassword=DigestUtils.md5Hex(registerRequest.getPassword());
-    authority.setId(2L);
-    authority.setNombre("cliente");
+    authority.setId(1L);
+    authority.setNombre("admin");
     user.setUsername(registerRequest.getUsername());
     user.setEmail(registerRequest.getEmail());
     user.setNombre(registerRequest.getNombre());
