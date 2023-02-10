d82213b7a0a3788d68376686a8d5ddfc716a10b4
josedavid-celsor <josedavidrosalesrios0310@gmail.com>
2023-02-10 12:48:50 +0100

    Pruebas auth

--- src/main/java/com/vFranco/vFranco/service/AuthService.java
+++ src/main/java/com/vFranco/vFranco/service/AuthService.java
@@ -48,7 +48,7 @@ public class AuthService implements UserDetailsService {
 
   public UsuarioEntity login(LoginRequest loginRequest) {
     UsuarioEntity usuario = usuarioRepository.findByUsername(loginRequest.getUsername());
-
+    System.out.print("EJECUTO ENTRADA METODO LOGIN");
     // Para encriptar el password
     String encryptedPassword=DigestUtils.md5Hex(loginRequest.getPassword());
     if (usuario == null) {
@@ -58,6 +58,7 @@ public class AuthService implements UserDetailsService {
       throw new RuntimeException("Password incorrect");
     }
     if(usuario.getVerificationCode() != null || !usuario.getVerificationCode().isEmpty() ||  !usuario.getVerificationCode().trim().isEmpty()){
+      
       throw new RuntimeException("You have to verify you account first, check your email");
     }
     UserDetails userDetails = loadUserByUsername(loginRequest.getUsername());
