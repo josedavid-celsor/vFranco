3603a0bad7a7a77e1cba9e9827039675423601ce
josedavid-celsor <josedavidrosalesrios0310@gmail.com>
2023-02-13 17:27:50 +0100

    Limpiando codigo

--- src/main/java/com/vFranco/vFranco/service/EmailService.java
+++ src/main/java/com/vFranco/vFranco/service/EmailService.java
@@ -15,15 +15,12 @@ public class EmailService {
   
     public void sendHtmlEmail(String email, String token) throws MessagingException {
       MimeMessage message = mailSender.createMimeMessage();
-      
-     /*  System.out.print(token); */
       message.setFrom(new InternetAddress("josedavid_celsor@outlook.es"));
       message.setRecipients(MimeMessage.RecipientType.TO, email);
       message.setSubject("Verification Email");
   
       String htmlContent = "<h1>This is the verification email</h1>" +
                            "<p>Copy the link vfranco.netlify.app/#/registro/" + token +" to verify</p>";
-     /*  System.out.print(htmlContent); */
       message.setContent(htmlContent, "text/html; charset=utf-8");
   
       mailSender.send(message);
@@ -31,15 +28,12 @@ public class EmailService {
 
   public void sendCode(String email, String token) throws MessagingException {
     MimeMessage message = mailSender.createMimeMessage();
-    
-   /*  System.out.print(token); */
     message.setFrom(new InternetAddress("josedavid_celsor@outlook.es"));
     message.setRecipients(MimeMessage.RecipientType.TO, email);
     message.setSubject("Verification Email");
 
     String htmlContent = "<h1>This is the recover password </h1>" +
                          "<p>Copy the link vfranco.netlify.app/#/recuperacion/"+ token +" to recover the password</p>";
-   /*  System.out.print(htmlContent); */
     message.setContent(htmlContent, "text/html; charset=utf-8");
 
     mailSender.send(message);
